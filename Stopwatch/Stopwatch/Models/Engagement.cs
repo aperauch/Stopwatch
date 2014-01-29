3bff5d74c852c580d6eb2c149e841cde15055a8e
Aron Aperauch <aperauch@gmail.com>
2014-01-29 16:04:38 -0500

    a

--- Stopwatch/Stopwatch/Models/Engagement.cs
+++ Stopwatch/Stopwatch/Models/Engagement.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Stopwatch.Models
 {
     public class Engagement
     {
+        [Key]
         public int EngagementID { get; set; }
+        //[ForeignKey("MemberID")]
         public int MemberID { get; set; }
+        //[ForeignKey("ProjectID")]
         public int ProjectID { get; set; }
+        //[DataType(DataType.Date)]
         public DateTime StartTime { get; set; }
-        public DateTime StoptTime { get; set; }
+        //[DataType(DataType.Date)]
+        public DateTime StopTime { get; set; }
         public double EngagementHours { get; set; }
+
+        public Engagement()
+        {
+            StartTime = DateTime.Now;
+            StopTime = StartTime;
+        }
     }
 }
